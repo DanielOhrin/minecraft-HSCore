6950fef3e3d0a1d50b62840840b5d5d0c10bb9be
Daniel Ohrin <xqiam@outlook.com>
2023-10-23 19:37:10 -0500

    Initial Commit

---
+++ src/main/java/net/highskiesmc/hscore/highskies/HSListener.java
@@ -0,0 +1,11 @@
+package net.highskiesmc.hscore.highskies;
+
+import org.bukkit.event.Listener;
+
+public class HSListener implements Listener {
+    private final HSPlugin main;
+
+    public HSListener(HSPlugin main) {
+        this.main = main;
+    }
+}
