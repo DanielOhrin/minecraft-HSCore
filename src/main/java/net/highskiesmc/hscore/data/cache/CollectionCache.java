06dfbe28de9c77648da469534fb244af97e33c39
Daniel Ohrin <xqiam@outlook.com>
2024-03-02 18:48:06 -0600

    Added Cache

---
+++ src/main/java/net/highskiesmc/hscore/data/cache/CollectionCache.java
@@ -0,0 +1,20 @@
+package net.highskiesmc.hscore.data.cache;
+
+import java.util.Collection;
+
+public abstract class CollectionCache<T extends Collection<?>> extends Cache<T> {
+    protected CollectionCache(T startingValue, T emptyOne, T emptyTwo) {
+        super(startingValue, emptyOne, emptyTwo);
+    }
+
+    @Override
+    protected void clearUpdateCache(boolean key) {
+        updateCache.get(key).clear();
+    }
+
+    @Override
+    public void dispose() {
+        updateCache.clear();
+        cache.clear();
+    }
+}
---
+++ src/main/java/net/highskiesmc/hscore/data/cache/MapCache.java
@@ -0,0 +1,20 @@
+package net.highskiesmc.hscore.data.cache;
+
+import java.util.Map;
+
+public abstract class MapCache<T extends Map<?, ?>> extends Cache<T> {
+    protected MapCache(T startingValue, T emptyOne, T emptyTwo) {
+        super(startingValue, emptyOne, emptyTwo);
+    }
+
+    @Override
+    protected void clearUpdateCache(boolean key) {
+        updateCache.get(key).clear();
+    }
+
+    @Override
+    public void dispose() {
+        updateCache.clear();
+        cache.clear();
+    }
+}
